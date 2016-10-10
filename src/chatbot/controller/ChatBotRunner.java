2d7f6673d22d55e272c6bb7ffa631b9a746ba384
themoffat28 <bthemoffat@gmail.com>
2016-10-10 09:33:06 -0600

    Created project and ignore files.

---
+++ src/chatbot/controller/ChatBotRunner.java
@@ -0,0 +1,10 @@
+package chatbot.controller;
+
+public class ChatBotRunner
+{
+	public static void main (String [] args)
+	{
+		ChatBotController chatBotController = new ChatBotController();
+		chatBotController.start();
+	}
+}
