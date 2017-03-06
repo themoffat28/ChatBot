1b36ed4d24a3159d2a9dcf53d988b147b3dbb5e7
themoffat28 <bthemoffat@gmail.com>
2017-03-06 09:54:37 -0700

    Began writing search twitter method.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -4,16 +4,24 @@ import chat.controller.ChatController;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.Twitter;
+import twitter4j.Status;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CTECTwitter
 {
 	private ChatController baseController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweets;
+	private List<String> ignoredWords;
 	
 	public CTECTwitter(ChatController baseController)
 	{
 		this.baseController = baseController;
+		searchedTweets = new ArrayList<Status>();
+		ignoredWords = new ArrayList<String>();
 		this.chatbotTwitter = TwitterFactory.getSingleton();
+		
 	}
 	
 	public void sendTweet(String tweet)
@@ -31,4 +39,20 @@ public class CTECTwitter
 			baseController.handleErrors(otherError);
 		}
 	}
+	
+	private void createIgnoredWordList()
+	{
+		
+	}
+	
+	private void collectTweets(String username)
+	{
+		
+	}
+	
+	public String getMostCommonWord()
+	{
+		return null;
+	}
 }
+
